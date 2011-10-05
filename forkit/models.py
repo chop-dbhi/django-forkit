f49aad88430bc9d05e90b9620d8cac2dcd16e222
Byron Ruth <bruth@codeomics.com>
2011-10-04 20:48:31 -0400

    Use function aliases

--- forkit/models.py
+++ forkit/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-from forkit.diff import diff_model_object
-from forkit.fork import fork_model_object, reset_model_object, commit_model_object
+from forkit import tools
 
 class ForkableModel(models.Model):
     "Convenience subclass which builds in the public Forkit utilities."
     def diff(self, *args, **kwargs):
-        return diff_model_object(self, *args, **kwargs)
+        return tools.diff(self, *args, **kwargs)
 
     def fork(self, *args, **kwargs):
-        return fork_model_object(self, *args, **kwargs)
+        return tools.fork(self, *args, **kwargs)
 
     def reset(self, *args, **kwargs):
-        return reset_model_object(self, *args, **kwargs)
+        return tools.reset(self, *args, **kwargs)
 
     def commit(self):
-        commit_model_object(self)
+        tools.commit(self)
 
     class Meta(object):
         abstract = True
